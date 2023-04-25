3a1e4a64052e29729bbba516588afe50f777961a
whydi <me.whyde@gmail.com>
2023-04-25 20:21:06 +0200

    Initial Commit

--- homework-8/main.py
+++ homework-8/main.py
@@ -1,39 +1,11 @@
-# a = int(input("from number:  "))
-# b = int(input("to number:  "))
-# c = a
-# d = True if input("even or odd:  ") == "even" else False
-# summ = 0
-#
-# while c <= b:
-#     if d and c % 2 == 0:
-#         summ += c
-#     elif not d and c %  2 == 1:
-#         summ += c
-#     c += 1
-# print(summ)
-
-
-# a = 0
-# b = int(input("number to:  "))
-#
-# while a <= b:
-#     if a % 3 == 0:
-#         print(a)
-#     a += 1
-#
-#
-
-
 
 f_number = 0
 s_number = int(input("Enter number:  "))
 
-while f_number != s_number:
+while f_number <= s_number:
     if f_number % 3 == 0:
         print(f_number)
     f_number += 1
-
-
 
 
 fromm = 0
