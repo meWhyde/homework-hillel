b14bbfd9cb778176650e6be1b0a73a7fbf36c2e0
whydi <me.whyde@gmail.com>
2023-05-11 11:02:13 +0200

    Initial Commit

---
+++ homework-12/main.py
@@ -0,0 +1,26 @@
+
+# 1 TypeError
+
+number = 1
+word = "hello"
+
+print(number + word)
+
+# 2 ImportError
+
+from crypt import doll
+
+# 3 ZeroDivisionError
+
+print(1 / 0)
+
+# 4 NameError
+
+name = "Andrei"
+print(age)
+
+# 5 ValueError
+
+first_list = [1, 5, 3, 78, 9]
+a,b,c = first_list
+print(a,b,c)
